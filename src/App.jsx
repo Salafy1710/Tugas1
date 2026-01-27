6cae1ea836ae6286fd23978f09bdf6cc8dddfb6d
Salafy1710 <salafy171009@student.abudzar.sch.id>
2026-01-27 11:50:06 +0700

    ganti teks

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,9 @@ function App() {
   return (
     <Layout>
       <div className="hero-section">
-        <h1>Selamat Datang di Website Modular</h1>
+        <h1>Halo</h1>
         <p>
-          Ini adalah contoh implementasi React dengan struktur folder yang rapi
-          dan menggunakan teknik Composition.
+          Ini contoh doang
         </p>
       </div>
     </Layout>
