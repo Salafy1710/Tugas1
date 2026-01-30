4830e8b044905170b6f31aa1e024dc26c2d0301e
Salafy1710 <salafy171009@student.abudzar.sch.id>
2026-01-30 09:59:37 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import Layout from './Layout.jsx';
 
-function App() {
-  return (
-    <Layout>
-      <div className="hero-section">
-        <h1>Halo</h1>
-        <p>
-          Ini contoh
-        </p>
-      </div>
-    </Layout>
-  );
-}
+// function App() {
+//   return (
+//     <Layout>
+//       <div className="hero-section">
+//         <h1>Halo</h1>
+//         <p>
+//           Ini contoh
+//         </p>
+//       </div>
+//     </Layout>
+//   );
+// }
 
 export default App;
